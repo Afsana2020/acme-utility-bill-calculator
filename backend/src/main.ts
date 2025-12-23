0746f074a6af0710a5c95fdf369439c881571e79
afsana <afsanahena24@gmail.com>
2025-12-23 23:21:53 +0600

    Fix: Update default route and CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,16 +4,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  // Enable CORS for your frontend
+
   app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'http://localhost:3001',
-      'https://ah-utility-bill-calculator.vercel.app/' 
-    ],
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true,
   });
   
   await app.listen(process.env.PORT || 5000);
+  console.log(`Application is running on port ${process.env.PORT || 5000}`);
 }
 bootstrap();
